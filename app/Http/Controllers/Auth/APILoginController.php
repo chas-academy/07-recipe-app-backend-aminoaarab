518aea934990ce32fd447cc59310404c8d38d353
Amin Aarab <amin.aarab@chasacademy.se>
2019-05-03 10:45:19 +0200

    Fixed so the register and login API works.

--- app/Http/Controllers/Auth/APILoginController.php
+++ app/Http/Controllers/Auth/APILoginController.php
@@ -1,39 +1,71 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
-
+namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Validator;
+use JWTFactory;
+use JWTAuth;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class APILoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function login(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password'=> 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $credentials = $request->only('email', 'password');
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        return response()->json(compact('token'));
     }
 }
+
+// namespace App\Http\Controllers\Auth;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+// class LoginController extends Controller
+// {
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Login Controller
+//     |--------------------------------------------------------------------------
+//     |
+//     | This controller handles authenticating users for the application and
+//     | redirecting them to your home screen. The controller uses a trait
+//     | to conveniently provide its functionality to your applications.
+//     |
+//     */
+
+//     use AuthenticatesUsers;
+
+//     /**
+//      * Where to redirect users after login.
+//      *
+//      * @var string
+//      */
+//     protected $redirectTo = '/home';
+
+//     /**
+//      * Create a new controller instance.
+//      *
+//      * @return void
+//      */
+//     public function __construct()
+//     {
+//         $this->middleware('guest')->except('logout');
+//     }
+// }
