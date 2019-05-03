994f23334403b737d76bebf1c1bcdb46eebdf970
Amin Aarab <amin.aarab@chasacademy.se>
2019-05-03 11:16:58 +0200

    include middleware protection

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
+Route::middleware('jwt.auth')->get('users', function(Request $request) {
+    return auth()->user();
+});
 Route::post('user/register', 'APIRegisterController@register');
 Route::post('user/login', 'APILoginController@login');
